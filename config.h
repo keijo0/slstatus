8e9e1b22e104f6fec928148f3296f649ea932b1a
keijo0 <95128521+keijo0@users.noreply.github.com>
2022-10-13 15:03:55 +0300

    Add files via upload

--- config.h
+++ config.h
@@ -3,8 +3,8 @@ static const char unknown_str[] = "n/a";
 #define MAXLEN 2048
 
 static const struct arg args[] = {
-//    	{ netspeed_rx, ": %sB/s | ",  "eno1" },
-//      { netspeed_tx, " : %sB/s | ",  "eno1" },
+    	//{ netspeed_rx, ": %sB/s | ",  "eno1" },
+      //{ netspeed_tx, " : %sB/s | ",  "eno1" },
 //	{ run_command, "%s | ", "acpi" },
 //	{ run_command, "tty: %s | ", "who | grep tty | wc -l" },
 //	{ wifi_perc, "%s | ", "wlan1" },
@@ -15,10 +15,9 @@ static const struct arg args[] = {
 	{ temp, "%s\u00b0C | ", "/sys/class/thermal/thermal_zone0/temp" },
 	{ ram_perc, " RAM %s%% | ", NULL	},
 	{ disk_perc, " ROOT %s%% | ", "/" },
-	{ disk_perc, " HDD %s%% | ", "/games" },
-	//{ disk_perc, " %s%%) | ", "/mnt/shit/games" },
+	//{ disk_perc, " HDD %s%% | ", "/mnt/games" },
 	//{ run_command, " PACKAGES %s | ", "qlist -I | wc -l" },
-	//{ uptime,   " UPTIME %s | ", NULL },
+	{ uptime,   "  UPTIME %s | ", NULL },
 	{ kernel_release, "%s | ", NULL },
 	{ datetime, 	" %s",    "%B %d %a %T" },
 };
